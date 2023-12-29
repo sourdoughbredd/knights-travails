aadee323cdff9a3117e03a3d89e270e782f4e024
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-12-29 10:50:23 -0500

    Add tests for shortest path function

--- test.js
+++ test.js
@@ -1,13 +1,31 @@
-import { KnightsTravails } from "./knights-travails.js";
+import { KnightsTravails as kt } from "./knights-travails.js";
 
 // Test adjacent nodes
 function testAdj(node) {
   console.log(
     `Adjacent to ${JSON.stringify(node)}: ${JSON.stringify(
-      KnightsTravails.adjacentNodes(node)
+      kt.adjacentNodes(node)
     )}`
   );
 }
 testAdj([0, 0]);
 testAdj([3, 3]);
-testAdj([0, 8]);
+// testAdj([0, 8]);
+
+// Test shortest path
+kt.knightMoves([0, 0], [0, 0]);
+kt.knightMoves([0, 0], [3, 3]);
+kt.knightMoves([3, 3], [0, 0]);
+kt.knightMoves([0, 0], [7, 7]);
+
+// Random test for errors
+// function randNumbersArray(n, min, max) {
+//   if (n == 0) return [];
+//   return Array.from({ length: n }, () => min + Math.floor(Math.random() * max));
+// }
+
+// for (let i = 0; i < 10000; i++) {
+//   const start = randNumbersArray(2, 0, 8);
+//   const end = randNumbersArray(2, 0, 8);
+//   kt.knightMoves(start, end);
+// }
